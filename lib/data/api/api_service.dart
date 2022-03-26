56d679b91c679c9824f817def6012ebd10bd1c93
Setyo Aryo Pangestu <setyoaryopangestu25@gmail.com>
2022-03-26 20:05:32 +0700

    update (salah paste)

--- lib/data/api/api_service.dart
+++ lib/data/api/api_service.dart
@@ -5,7 +5,7 @@ import '../model/article.dart';
 
 class ApiService {
   static const String _baseUrl = "https://newsapi.org/v2/";
-  static const String _apiKey = "815bf36d08f44317a0fde54b151c6fc6";
+  static const String _apiKey = "1a99fa65b4a243a2bd52bcd3e1098aee";
   static const String _country = 'id';
 
   Future<Articles> topHeadline() async {
